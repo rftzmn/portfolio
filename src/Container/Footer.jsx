8dede2f7aef3c46d46dc76f40427f6bacc59cb95
Ashaduzzaman Rifat <rftzmn1@gmail.com>
2024-01-28 00:48:24 +0600

    email changed

--- src/Container/Footer.jsx
+++ src/Container/Footer.jsx
@@ -45,8 +45,8 @@ const Footer = () => {
       <div className="app__footer-cards">
         <div className="app__footer-card">
           <img src={images.email} alt="email" />
-          <a href="mailto:rftzmn1@gmail.com" className="p-text">
-            rftzmn1@gmail.com
+          <a href="mailto:rifat_zaman@outlook.com" className="p-text">
+            rifat_zaman@outlook.com
           </a>
         </div>
 
